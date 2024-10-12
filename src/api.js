268994e5567443dd8ed7edc046e6254bf04a8352
Mohammed Azhar Uddin <mohammedazharuddin@Mohammeds-MacBook-Air.local>
2024-10-12 17:30:47 +0530

    dynamic variable

--- src/api.js
+++ src/api.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-const SearchImages = async () => {
+const SearchImages = async (term) => {
   const response = await axios.get(`${import.meta.env.VITE_URL}search/photos`, {
     headers: {
       Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
 
-  return response;
+  console.log(response.data.results);
+
+  return response.data.results;
 };
 
 export default SearchImages;
